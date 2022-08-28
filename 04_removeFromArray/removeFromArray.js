18b7cd873645733be27df5c19f39426a85b856fd
kckuei <kevinkuei@gmail.com>
2022-08-27 17:59:35 -0700

    Implement solution for problems 01 to 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...items) {
 
+    for (const item of items) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === item) {
+                array.splice(i, 1)
+                break;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
