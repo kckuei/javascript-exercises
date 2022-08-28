df8f6983200d3933932c24d619a2b580033cabf3
kckuei <kevinkuei@gmail.com>
2022-08-27 18:59:15 -0700

    Implement 06-learpyears solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function (year) {
 
+    const divisBy4 = (year % 4 == 0);
+    const divisBy100 = (year % 100 == 0);
+    const divisBy400 = (year % 400 == 0);
+
+    if (divisBy4 && divisBy100 && divisBy400) {
+        return true;
+    }
+    if (divisBy4 && !divisBy100) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
