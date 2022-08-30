de7aea5329f5767ad77e18bc6ed9d16206ec5b08
kckuei <kevinkuei@gmail.com>
2022-08-29 17:22:05 -0700

    Implement solution 08-calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((previousValue, currentValue) => previousValue + currentValue,
+    0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((previousValue, currentValue) => previousValue * currentValue,
+    1);
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num <= 1) {
+    return 1;
+  }
+  result = 1;
+  for (let i = 1; i < num + 1; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
