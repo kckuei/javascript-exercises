a916110b864e1b74970a8bfa23997f8c23265b4f
kckuei <kevinkuei@gmail.com>
2022-08-31 21:21:02 -0700

    Implement solution to 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replace(/\s+/g, '');
+    string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+    string = string.toLowerCase();
+    for (let i = 0; i < parseInt(string.length / 2); i++) {
+        if (string[i] !== string[string.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
