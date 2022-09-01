2da2b4ce8b1bf1862e7017be2d7e161c152e62d6
kckuei <kevinkuei@gmail.com>
2022-08-31 22:01:03 -0700

    Implements solution to 11-getthetitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (array) {
+    return array.map(book => {
+        return book.title
+    })
 };
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
